401af5e6d662fad628b4df284deee3bbf01231e1
Rebeca Brito <rbrito.admin@MANDQIJRMDY63.corp.eldorado.org.br>
2025-10-07 17:56:59 -0400

    Atualização do projeto: ajustes no player

--- app/src/main/java/com/example/musicplayer/MusicPlayerScreen.kt
+++ app/src/main/java/com/example/musicplayer/MusicPlayerScreen.kt
@@ -1,8 +1,12 @@
 import android.content.Context
 import android.media.MediaPlayer
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.musicplayer.R
 
 @Composable
@@ -12,4 +16,38 @@ fun MusicPlayerScreen(context: Context) {
     }
 
     var isPlaying by remember { mutableStateOf(false) }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            mediaPlayer.release()
+        }
+    }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            if (!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+                isPlaying = true
+            }
+        }) {
+            Text("Play")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+                isPlaying = false
+            }
+        }) {
+            Text("Pause")
+        }
+    }
 }
