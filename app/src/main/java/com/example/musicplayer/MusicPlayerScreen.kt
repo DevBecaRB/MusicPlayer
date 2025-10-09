f08b06acb161451a1eeb92059370cca73ab6f3a8
Rebeca Brito <98970380+DevBecaRB@users.noreply.github.com>
2025-10-09 10:15:33 -0400

    Mudança na estrutura e adição da barra de progresso

--- app/src/main/java/com/example/musicplayer/MusicPlayerScreen.kt
+++ app/src/main/java/com/example/musicplayer/MusicPlayerScreen.kt
@@ -1,21 +1,72 @@
 import android.content.Context
 import android.media.MediaPlayer
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.musicplayer.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun MusicPlayerScreen(context: Context) {
-    val mediaPlayer = remember {
-        MediaPlayer.create(context, R.raw.sobrenome)
-    }
+    val musicList = listOf(
+       R.raw.sobrenome to "Sobrenome",
+        R.raw.fatoconsumado to "Fato Consumado",
+        R.raw.sentadamalvada to "Sentada Malvada"
+    )
 
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Spacer(modifier = Modifier.height(80.dp))
+
+        Image(
+            painter = painterResource(R.drawable.baseline_music_note_24),
+            contentDescription = "logo",
+            modifier = Modifier.size(120.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text("The Trending Songs", style = MaterialTheme.typography.headlineMedium)
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        musicList.forEach { (resId, title) ->
+
+            MusicItem(context = context, resId = resId, title = title)
+
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
+}
+
+@Composable
+fun MusicItem(context: Context, resId: Int, title: String){
+    val mediaPlayer = remember { MediaPlayer.create(context, resId) }
     var isPlaying by remember { mutableStateOf(false) }
+    var progress by remember { mutableFloatStateOf(0f) }
+
+    LaunchedEffect(isPlaying) {
+        while (isPlaying && mediaPlayer.isPlaying){
+            progress = mediaPlayer.currentPosition.toFloat() / mediaPlayer.duration
+            delay(500)
+        }
+    }
 
     DisposableEffect(Unit) {
         onDispose {
@@ -23,31 +74,35 @@ fun MusicPlayerScreen(context: Context) {
         }
     }
 
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(32.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            if (!mediaPlayer.isPlaying) {
-                mediaPlayer.start()
-                isPlaying = true
-            }
-        }) {
-            Text("Play")
-        }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(title, style = MaterialTheme.typography.titleMedium)
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
+
+        LinearProgressIndicator(
+        progress = progress,
+        modifier = Modifier
+            .fillMaxWidth(0.8f)
+            .height(12.dp)
+            .clip(RoundedCornerShape(4.dp)),
+        color = ProgressIndicatorDefaults.linearColor,
+        trackColor = ProgressIndicatorDefaults.linearTrackColor,
+        strokeCap = ProgressIndicatorDefaults.LinearStrokeCap
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
 
         Button(onClick = {
             if (mediaPlayer.isPlaying) {
                 mediaPlayer.pause()
                 isPlaying = false
+            } else {
+                mediaPlayer.start()
+                isPlaying = true
             }
         }) {
-            Text("Pause")
+            Text(if (isPlaying) "Pause" else "Play")
         }
     }
+
 }
